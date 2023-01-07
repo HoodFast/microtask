d365eb3aab06d2f6738b4217b5d13b96e5e46599
HoodFast <rabota-trassa@mail.ru>
2023-01-07 17:11:36 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {NewComponent} from "./site/NewComponent";
+
+
+
+export type CarsType = {
+    manufacturer: string
+    model: string
+}
+
+const topCars: Array<CarsType> = [
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mercedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'}
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <NewComponent cars={topCars}/>
+    );
 }
 
 export default App;
---
+++ src/site/NewComponent.tsx
@@ -0,0 +1,24 @@
+import {CarsType} from "../App";
+
+
+export type propsCarsType = {
+    cars: Array<CarsType>
+}
+
+
+export const NewComponent = (props: propsCarsType) => {
+    return (
+        <>
+            {
+
+            }
+            {props.cars.map((s,index)=>{
+                return <li key={index}>
+                    <div> {index+1}</div>
+                    <span>{s.manufacturer}</span>
+                    <span>-- model: {s.model}</span>
+                </li>
+            })}
+        </>
+    )
+}
